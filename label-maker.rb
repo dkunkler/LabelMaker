5f15251eb2d2fc59679ff6e8aff6aa9dc5daa690
Rettdog <rettdog@gmail.com>
2020-07-25 17:13:58 -0700

    Added descriptions and file reading

--- label-maker.rb
+++ label-maker.rb
@@ -1,33 +1,10 @@
-#!/usr/bin/env ruby
+ #!/usr/bin/env ruby
 require "yaml"
 require "octokit"
 require "dotenv"
+require "yaml"
 Dotenv.load
-
-LABELS = {
-  "1-WIP"       => "ffe6c9",
-  "2-WIP"       => "ffe6c9",
-  "3-WIP"       => "ffe6c9",
-  "4-WIP"       => "ffe6c9",
-  "5-WIP"       => "ffe6c9",
-  "Blocked"     => "f46164",
-  "Chaos"       => "95def4",
-  "Compute Foundation" => "e7c0f9",
-  "Data Infrastructure" => "e99695",
-  "Data Pilelines" => "b2ffc5",
-  "Database Infrastructure" => "72f957",
-  "Git Systems Protocols" => "fcf56f",
-  "Git Systems Storage" => "cccc00",
-  "Git Core" => "ee8800",
-  "Observability" => "cce0ff",
-  "Physical Infrastructure" => "713dbf",
-  "SRE-Americas" => "80b3ff",
-  "SRE-EMEA" => "0066ff",
-  "Status: Green" => "00cc00",
-  "Status: Red" => "ff0000",
-  "Status: Yellow" => "ffff00"
-
-}
+LABELS = YAML.load(File.read("labels.yml"))
 
 client ||= Octokit::Client.new(
   :login        => ENV.fetch("GH_USER", ENV["USER"]),
@@ -44,10 +21,12 @@ end
 #adding "Blocked" label to a repo that already has "blocked" label.
 
 labels = client.labels(repo)
-LABELS.each do |name, color|
+p stuff
+LABELS.each do |name, details|
   if labels.select {|l| l.name.downcase==name.downcase}.empty?
-    puts "Adding #{name} (\##{color})"
-    client.add_label(repo, name, color)
+    #puts "Adding #{name} (\##{color})"
+    puts "Adding #{name} with color \##{details["color"]} and description #{details["description"]}"
+    client.add_label(repo, name, details["color"], options = {"description" => details["description"]} )
   else
     puts "Skipping #{name} already exists"
   end
